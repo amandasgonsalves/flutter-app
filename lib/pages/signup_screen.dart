21d2753efcf98244fe3e075255de23bb6df0bcb5
amandasgonsalves <105944287+amandasgonsalves@users.noreply.github.com>
2024-12-09 20:18:09 -0300

    . teste commit

--- lib/pages/signup_screen.dart
+++ lib/pages/signup_screen.dart
@@ -8,7 +8,7 @@ class SignupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Cadastro')),
+      appBar: AppBar(title: Text('Cadastro1')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
