32a33ddb1319a0a57e39d27a3c01ab776a971f80
amandasgonsalves <105944287+amandasgonsalves@users.noreply.github.com>
2025-02-09 16:39:04 -0300

    login

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -2,18 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:crypto/crypto.dart'; // Para hash de senha
 import 'dart:convert';
-import 'home_screen.dart'; // Certifique-se de que esse arquivo esteja presente
+import 'home_screen.dart';
+import 'signup_screen.dart'; // Certifique-se de que o SignupScreen está importado corretamente
 
 class LoginScreen extends StatelessWidget {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  // Função para realizar o login
   void _login(BuildContext context) async {
     String username = _usernameController.text.trim();
     String password = _passwordController.text.trim();
 
-    // Validação de e-mail e senha
     if (!_isValidEmail(username)) {
       _showErrorDialog(context, 'Por favor, insira um e-mail válido.');
       return;
@@ -24,14 +23,10 @@ class LoginScreen extends StatelessWidget {
       return;
     }
 
-    // Abertura da box no Hive
     var box = await Hive.openBox('users');
-
-    // Recuperando o usuário
     var user = box.get(username);
 
     if (user != null) {
-      // Verifica se o hash da senha fornecida corresponde ao hash armazenado
       String hashedPassword = _hashPassword(password);
 
       if (user['password'] == hashedPassword) {
@@ -54,18 +49,15 @@ class LoginScreen extends StatelessWidget {
     }
   }
 
-  // Função para verificar o formato do e-mail
   bool _isValidEmail(String email) {
     final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
     return emailRegex.hasMatch(email);
   }
 
-  // Função para validar a senha
   bool _isValidPassword(String password) {
     return password.length >= 6;
   }
 
-  // Função para mostrar o erro
   void _showErrorDialog(BuildContext context, String message) {
     showDialog(
       context: context,
@@ -84,10 +76,9 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  // Função para criar hash da senha
   String _hashPassword(String password) {
-    var bytes = utf8.encode(password); // Codificando senha
-    var digest = sha256.convert(bytes); // Gerando hash SHA256
+    var bytes = utf8.encode(password);
+    var digest = sha256.convert(bytes);
     return digest.toString();
   }
 
@@ -103,7 +94,13 @@ class LoginScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Campo de e-mail (ou nome de usuário)
+            Center(
+              child: Image.asset(
+                'assets/flash_card.png',
+                height: 100,
+              ),
+            ),
+            SizedBox(height: 35),
             TextField(
               controller: _usernameController,
               decoration: InputDecoration(
@@ -113,11 +110,12 @@ class LoginScreen extends StatelessWidget {
                 ),
                 filled: true,
                 fillColor: Color(0xFFE8F5FA),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
               ),
               style: TextStyle(fontFamily: 'Poppins'),
             ),
             SizedBox(height: 16.0),
-            // Campo de senha
             TextField(
               controller: _passwordController,
               decoration: InputDecoration(
@@ -127,12 +125,13 @@ class LoginScreen extends StatelessWidget {
                 ),
                 filled: true,
                 fillColor: Color(0xFFE8F5FA),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
               ),
               obscureText: true,
               style: TextStyle(fontFamily: 'Poppins'),
             ),
             SizedBox(height: 20),
-            // Botão de login
             ElevatedButton(
               onPressed: () => _login(context),
               child: Text('Login', style: TextStyle(fontFamily: 'Poppins')),
@@ -141,6 +140,25 @@ class LoginScreen extends StatelessWidget {
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 30.0),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(30.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            // Botão de navegação para a tela de cadastro
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          SignupScreen()), // Certifique-se de que SignupScreen está importado
+                );
+              },
+              child: Text(
+                'Não tem uma conta? Cadastre-se!',
+                style: TextStyle(
+                  color: Color(0xFF4A90E2),
+                  fontFamily: 'Poppins',
                 ),
               ),
             ),
